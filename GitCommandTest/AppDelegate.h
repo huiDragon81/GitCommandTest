3d727eac42dbabc725c4f00e0dcc36b51038efad
vendys-huiyong <hui@vendys.co.kr>
2017-09-04 17:20:13 +0900

    1

---
+++ GitCommandTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitCommandTest
+//
+//  Created by vendys on 2017. 9. 4..
+//  Copyright © 2017년 vendys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitCommandTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitCommandTest
+//
+//  Created by vendys on 2017. 9. 4..
+//  Copyright © 2017년 vendys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
