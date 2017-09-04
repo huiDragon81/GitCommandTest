05d0d3e2a6607aaac56a8b35ca30d4f34c1f76b6
vendys-huiyong <hui@vendys.co.kr>
2017-09-04 17:22:13 +0900

    test1

--- GitCommandTest/AppDelegate.m
+++ GitCommandTest/AppDelegate.m
@@ -16,6 +16,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // test1
+    // test1
+    // test1
+    // test1
+    // test1
     return YES;
 }
 
